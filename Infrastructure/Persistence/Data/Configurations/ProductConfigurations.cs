a74d18513903047ddb05a81ac001868872ebb1d8
Youssef Ahmed <youssefahmed4657@gmail.com>
2025-09-05 19:41:54 +0300

    feat(products): add entity configurations Configured relationships and constraints for Product, ProductBrand, ProductType

---
+++ Infrastructure/Persistence/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,17 @@
+﻿namespace Persistence.Data.Configurations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p => p.ProductBrand)
+                .WithMany().HasForeignKey(p => p.BrandId);
+
+            builder.HasOne(p => p.ProductType)
+                .WithMany().HasForeignKey(p => p.TypeId);
+
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(10,2)");
+        }
+    }
+}
